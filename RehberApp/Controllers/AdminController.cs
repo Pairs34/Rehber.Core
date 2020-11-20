873670772e359a12182734f221937ddd88d2f853
Pairs34 <34889012+Pairs34@users.noreply.github.com>
2020-11-20 21:47:34 +0300

    x

---
+++ RehberApp/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RehberApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
